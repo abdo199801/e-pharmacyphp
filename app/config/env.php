1f23f53c4f6ce88a70cf1307bf31213715df2b91
Abdellah Baqba <abdobaq777@gmail.com>
2025-12-25 16:42:31 +0100

    dd

--- app/config/env.php
+++ app/config/env.php
@@ -7,14 +7,8 @@ return [
         'url' => 'http://localhost/pharmaplatform',
         'timezone' => 'UTC'
     ],
-    'email' => [
-        'smtp_host' => 'smtp.gmail.com',
-        'smtp_port' => 587,
-        'smtp_username' => 'abdobaq777@gmail.com',
-        'smtp_password' => 'eteo zyja tbvd ckfo', // You'll generate this
-        'smtp_secure' => 'tls',
-        'from_email' => 'abdobaq777@gmail.com',
-        'from_name' => 'PharmaPlatform'
+    'email' => [ 
+       
     ],
     'app' => [
         'base_url' => 'http://localhost/pharmacy-platform',
